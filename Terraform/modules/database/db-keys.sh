091e2a88ba9b6238cdb920c61e6932b0ad3e7e55
Isakova Svitlana <Isakova Svitlana>
2019-10-20 12:14:46 +0000

    Have changed db-keys.sh / pd-keys.sh

--- Terraform/modules/database/db-keys.sh
+++ Terraform/modules/database/db-keys.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-cat /home/isakovasvitlana/ans_db.pub >> /home/isakovasvitlana/.ssh/authorized_keys
+sudo useradd buildserver
+sudo mkdir /home/buildserver/.ssh
+sudo cat /home/isakovasvitlana/ans_db.pub >> /home/buildserver/.ssh/authorized_keys
+sudo chmod 600 /home/buildserver/.ssh/authorized_keys
+sudo chown -R buildserver:buildserver /home/buildserver 
 sudo rm /home/isakovasvitlana/ans_db.pub
 sudo rm /home/isakovasvitlana/db-keys.sh
 
