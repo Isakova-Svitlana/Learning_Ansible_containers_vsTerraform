219e010819da3558d88dfcac2ff289adaf34a9c3
Isakova Svitlana <Isakova Svitlana>
2019-11-14 19:39:38 +0000

    Adding new path

--- scripts/ansible_carts.sh
+++ scripts/ansible_carts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd /opt/git/Learning_Ansible_containers_vsTerraform/Ansible/
-ansible-playbook carts_db.yaml
+#cd /opt/git/Learning_Ansible_containers_vsTerraform/Ansible/
+ansible-playbook carts_playbook/Ansible/carts_db.yaml
